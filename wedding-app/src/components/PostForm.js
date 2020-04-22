e18fc47dfc70b0c8d3920bd12393ccc06a3073ff
Sydney Crumley <srsimps19@gmail.com>
2020-04-22 18:10:21 -0500

    fixed a form i forgot

--- wedding-app/src/components/PostForm.js
+++ wedding-app/src/components/PostForm.js
@@ -3,6 +3,13 @@ import { connect } from 'react-redux';
 import { addNewPost } from './actions';
 
 
+var section = {
+    'display': 'flex',
+    'justify-content': 'center',
+}
+var form = {
+    'width': '50%'
+}
 
 class PostForm extends React.Component {
     state = {
@@ -45,55 +52,85 @@ class PostForm extends React.Component {
 
     render() {
         return (
-            <div className="postForm">
-                <form onSubmit={this.addPost}>
-                <input
-                        type="text"
-                        name="eventname"
-                        placeholder="Name of the Wedding!"
-                        value={this.state.post.eventname}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="date"
-                        placeholder="Date of the Wedding!"
-                        value={this.state.post.date}
-                        onChange={this.handleChange}
-                    />
-                    <textarea
-                        type="text"
-                        name="description"
-                        placeholder="Description of the Wedding!"
-                        value={this.state.post.description}
-                        onChange={this.handleChange}
-                        rows="5"
-                    />
-                    <input
-                        type="text"
-                        name="location"
-                        placeholder="Location of the Wedding!"
-                        value={this.state.post.location}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="theme"
-                        placeholder="Wedding Theme!"
-                        value={this.state.post.theme}
-                        onChange={this.handleChange}
-                    />
-                    <textarea
-                        type="text"
-                        name="vendors"
-                        placeholder="List of Vendors Used!"
-                        value={this.state.post.vendors}
-                        onChange={this.handleChange}
-                        rows="5"
-                    />
-                    <button>Post Wedding!</button>
+            <section class="section has-text-centered" style={section}>
+                <form onSubmit={this.addPost} style={form}>
+                <div class="field">
+                        <div class="control">
+                            <input
+                                class="input is-primary"
+                                type="text"
+                                name="eventname"
+                                placeholder="Name of the Wedding!"
+                                value={this.state.post.eventname}
+                                onChange={this.handleChanges}
+                            />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input
+                                class="input is-primary"
+                                type="text"
+                                name="date"
+                                placeholder="Date of the Wedding!"
+                                value={this.state.post.date}
+                                onChange={this.handleChanges}
+                            />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <textarea
+                                class="textarea is-primary"
+                                type="text"
+                                name="description"
+                                placeholder="Description of the Wedding!"
+                                value={this.state.post.description}
+                                onChange={this.handleChanges}
+                                rows="5"
+                            />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input
+                                class="input is-primary"
+                                type="text"
+                                name="location"
+                                placeholder="Location of the Wedding!"
+                                value={this.state.post.location}
+                                onChange={this.handleChanges}
+                            />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <input
+                                class="input is-primary"
+                                type="text"
+                                name="theme"
+                                placeholder="Wedding Theme!"
+                                value={this.state.post.theme}
+                                onChange={this.handleChanges}
+                            />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <textarea
+                                class="textarea is-primary"
+                                type="text"
+                                name="vendors"
+                                placeholder="List of Vendors Used!"
+                                value={this.state.post.vendors}
+                                onChange={this.handleChanges}
+                                rows="5"
+                            />
+                        </div>
+                    </div>
+                    <button class="button is-primary">Post Wedding!</button>
                 </form>
-            </div>
+            </section>
             
         );
     }  
